f10355811b55b821d3801b5cb162b79223ebe198
Mahesh Alyana <alianamahesh@gmail.com>
2025-05-21 05:10:24 +0530

    verifying adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,8 @@ export default function RootLayout({
         <meta property="og:image:height" content="630" />
         <meta property="og:image:alt" content="Taskin Studio" />
       </head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2234156203417980"
+        crossOrigin="anonymous"></script>
       <body className={`${inter.variable} font-sans antialiased flex flex-col min-h-screen`}>
         <ThemeProvider
           attribute="class"
